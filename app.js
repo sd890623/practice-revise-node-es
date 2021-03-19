ea68bc08e96e3d76a6fccec80c8970fba6cba3d4
sd890623 <sd890623@gmail.com>
2021-03-19 15:04:46 +1100

    Initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+import {mylog} from "./logger";
+const logger = require('./logger')
+logger.logA('New world');
+
+const doStuff = data => {
+    return new Promise((resolve, reject) => {
+        if (data === true) {
+            resolve({status: 'success'});
+        } else {
+            reject({status: 'fail'});
+        }
+    })
+};
+
+doStuff(true).then(
+    msg => {
+        console.log(`first${msg.status}`);
+        return doStuff(true);
+    }
+).then(
+    msg => {
+        console.log(`2nd${msg.status}`);
+        return doStuff(false);
+    }
+).
+catch(
+    msg => {
+        console.log(msg);
+    }
+)
+
---
+++ logger.js
@@ -0,0 +1,7 @@
+export const mylog = msg => {
+    console.log(`This is the message: ${msg}`);
+}
+
+module.exports = {
+    logA: mylog
+}
